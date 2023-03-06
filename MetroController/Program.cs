247e3fc1a39a4c0f5534a7c192a63191d531eb44
tigerros <tigerros.gh@gmail.com>
2023-03-06 19:29:17 +0100

    Add project files.

---
+++ MetroController/Program.cs
@@ -0,0 +1,36 @@
+﻿using Sandbox.ModAPI.Ingame;
+using IngameScript.Services;
+
+namespace IngameScript {
+	class Program : MyGridProgram {
+		private readonly PropulsionService _propulsionService;
+
+		public Program() {
+			_propulsionService = new PropulsionService(this);
+			Runtime.UpdateFrequency = UpdateFrequency.None;
+		}
+
+		public void Main(string argument, UpdateType updateSource) {
+			if (updateSource == UpdateType.Update10) {
+				//_propulsionService.FinalizeVelocityChange();
+				_propulsionService.Update();
+				return;
+			}
+
+			switch (argument) {
+				case "speed turn":
+					_propulsionService.ChangeVelocityTo(PropulsionService.TurnVelocity);
+					break;
+				case "speed straight":
+					_propulsionService.ChangeVelocityTo(PropulsionService.StraightVelocity);
+					break;
+				case "speed prestop":
+					_propulsionService.ChangeVelocityTo(PropulsionService.PreStopVelocity);
+					break;
+				case "speed stop":
+					_propulsionService.ChangeVelocityTo(0);
+					break;
+			}
+		}
+	}
+}
